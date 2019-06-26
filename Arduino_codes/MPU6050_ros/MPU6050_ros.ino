b10c3cf47be9bb49d727afccdc8c2c1c94dc2209
manthan99 <manthanhemangpatel@gmail.com>
2019-06-26 14:07:45 -0400

    recovered packages

---
+++ Arduino_codes/MPU6050_ros/MPU6050_ros.ino
@@ -0,0 +1,161 @@
+#include <ros.h>
+#include <geometry_msgs/Twist.h>
+
+ros::NodeHandle nh;
+geometry_msgs::Twist rpy_data;
+
+ros::Publisher rpy_publisher("raw_orientation", &rpy_data);
+
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 mpu;
+//MPU6050 mpu(0x69); // <-- use for AD0 high
+
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+
+
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+
+
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    nh.initNode();
+    nh.advertise(rpy_publisher);
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        mpu.setDMPEnabled(true);
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+        dmpReady = true;
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+   
+    }
+
+    // configure LED for output
+    pinMode(LED_PIN, OUTPUT);
+}
+
+
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+
+void loop() {
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+        rpy_publisher.publish(&rpy_data);
+        nh.spinOnce();
+        delay(5);
+    }
+
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        //Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            
+            rpy_data.angular.z = (ypr[0] * 180/M_PI);
+            rpy_data.angular.y = (ypr[1] * 180/M_PI);
+            rpy_data.angular.x = (ypr[2] * 180/M_PI);
+        #endif
+        
+        // blink LED to indicate activity
+        blinkState = !blinkState;
+        digitalWrite(LED_PIN, blinkState);
+    }
+}
---
+++ Arduino_codes/jetsoncar/jetsoncar.ino
@@ -0,0 +1,92 @@
+/*
+  Arduino ROS node for JetsonCar project
+  The Arduino controls a TRAXXAS Rally Car
+  MIT License
+  JetsonHacks (2016)
+*/
+
+#if defined(ARDUINO) && ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include <WProgram.h>
+#endif
+
+#include <Servo.h> 
+#define USB_USBCON
+#include <ros.h>
+#include <std_msgs/UInt16.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Int32.h>
+#include <std_msgs/Empty.h>
+#include <geometry_msgs/Twist.h>
+
+ros::NodeHandle  nodeHandle;
+// These are general bounds for the steering servo and the
+// TRAXXAS Electronic Speed Controller (ESC)
+const int minSteering = 30 ;
+const int maxSteering = 150 ;
+const int minThrottle = 0 ;
+const int maxThrottle = 150 ;
+
+Servo steeringServo;
+Servo electronicSpeedController ;  // The ESC on the TRAXXAS works like a Servo
+
+std_msgs::Int32 str_msg;
+ros::Publisher chatter("chatter", &str_msg); 
+
+// Arduino 'map' funtion for floating point
+double fmap (double toMap, double in_min, double in_max, double out_min, double out_max) {
+  return (toMap - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void driveCallback ( const geometry_msgs::Twist&  data )
+{
+  
+//  int steeringAngle = fmap(twistMsg.angular.z, -1.0, 1.0, minSteering, maxSteering) ;
+//  // The following could be useful for debugging
+//  // str_msg.data= steeringAngle ;
+chatter.publish(&str_msg);
+//  // Check to make sure steeringAngle is within car range
+//  if (steeringAngle < minSteering) { 
+//    steeringAngle = minSteering;
+//  }
+//  if (steeringAngle > maxSteering) {
+//    steeringAngle = maxSteering ;
+//  }
+  steeringServo.write(data.angular.z) ;
+  delay(10);
+  str_msg.data = data.linear.x;
+  if(data.linear.x<85 || data.linear.x>95)
+  electronicSpeedController.write(data.linear.x);
+  else if(data.linear.x == 90)
+  electronicSpeedController.write(data.linear.x);
+  digitalWrite(13, HIGH-digitalRead(13));  //toggle led  
+  chatter.publish(&str_msg);
+ 
+}
+ 
+ros::Subscriber<geometry_msgs::Twist> driveSubscriber("/cmd_vel1", &driveCallback) ;
+
+void setup(){
+  pinMode(13, OUTPUT);
+  Serial.begin(57600) ;
+  nodeHandle.initNode();
+  // This can be useful for debugging purposes
+  nodeHandle.advertise(chatter);
+  // Subscribe to the steering and throttle messages
+  nodeHandle.subscribe(driveSubscriber) ;
+  // Attach the servos to actual pins
+  steeringServo.attach(9); // Steering servo is attached to pin 9
+  electronicSpeedController.attach(11); // ESC is on pin 11
+  // Initialize Steering and ESC setting
+  // Steering centered is 90, throttle at neutral is 90
+  steeringServo.write(95) ;
+  electronicSpeedController.write(90) ;
+  delay(1000) ;
+  
+}
+
+void loop(){
+  nodeHandle.spinOnce();
+  delay(10);
+}
